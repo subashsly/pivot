20cfc652a5c71e05791213fe01cd779abfcc2b5f
Subash Shahi <subashsly33@gmail.com>
2017-04-11 18:26:16 +0545

    final

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ config.serve_static_assets = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.precompile += %w(*.js)
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
