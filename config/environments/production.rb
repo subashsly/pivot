e0e193f8e85286774590816fb649ae1ebd203248
Subash Shahi <subashsly33@gmail.com>
2017-05-10 11:25:17 +0545

    final

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,8 @@ config.serve_static_assets = false
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-   config.serve_static_files = true
+   # config.serve_static_files = true
+   config.public_file_server.enabled = true
 
 
   # Compress JavaScripts and CSS.
