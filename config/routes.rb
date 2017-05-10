6ed04a7053bd3bb67ceb67442cf88c46c376f575
Subash Shahi <subashsly33@gmail.com>
2017-05-10 11:51:00 +0545

    final

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   # devise_for :users 
   devise_scope :user do
   	get 'login', to: 'devise/sessions#new'
-
+    put 'users' => 'devise/registrations#update', as: 'user_registration'
+  get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
+  delete 'users' => 'devise/registrations#destroy', as: 'registration'
   	# get 'login', to: 'student_galleries#index'
   	
 end
